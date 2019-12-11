8801abb37188468604610ad0866089bb93181ea7
Camarchepas <36743875+Camarchepas@users.noreply.github.com>
2019-12-11 11:27:34 +0100

    Updates

---
+++ precache-manifest.9e2e528969189f7b1d8f38dc93223592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db4755fcde13f285814f2ab18d81cc5",
+    "url": "/PutzplanApp/index.html"
+  },
+  {
+    "revision": "bffcbc8dcaa9f725bb98",
+    "url": "/PutzplanApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3cacb73b14f75801a4b",
+    "url": "/PutzplanApp/static/js/2.080bfb15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PutzplanApp/static/js/2.080bfb15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bffcbc8dcaa9f725bb98",
+    "url": "/PutzplanApp/static/js/main.1f2a98e3.chunk.js"
+  },
+  {
+    "revision": "5f7e9672159afc5a52bb",
+    "url": "/PutzplanApp/static/js/runtime-main.cf5147f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PutzplanApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
